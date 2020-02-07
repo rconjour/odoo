df6ff4da6bd3abd5c1d00891f55f47a04d34c69c
Robin Conjour <rconjour@MacBook-Pro-van-Robin.local>
2020-02-07 15:21:43 +0100

    Add vat check NL

--- addons/base_vat/base_vat.py
+++ addons/base_vat/base_vat.py
@@ -256,6 +256,54 @@ class res_partner(osv.osv):
         #Valid format and valid date
         return True
 
+    __check_vat_nl_re = re.compile("(?:NL)?[0-9A-Z+*]{10}[0-9]{2}")
+
+    def check_vat_nl(self, vat):
+        """
+        VAT validation to support the new format introduced in January 2020,
+        """
+
+        try:
+            from stdnum.util import clean
+            from stdnum.nl.bsn import checksum
+        except ImportError:
+            return True
+
+        vat = "NL" + vat
+        vat = clean(vat, ' -.').upper().strip()
+        if not (len(vat) == 14):
+            return False
+
+        # Check the format
+        match = self.__check_vat_nl_re.match(vat)
+        if not match:
+            return False
+
+        # Match letters to integers
+        char_to_int = {k: str(ord(k) - 55) for k in string.ascii_uppercase}
+        char_to_int['+'] = '36'
+        char_to_int['*'] = '37'
+
+        # Remove the prefix
+        vat = vat[2:]
+
+        # 2 possible checks:
+        # - For natural persons
+        # - For non-natural persons and combinations of natural persons (company)
+
+        # Natural person => mod97 full checksum
+        check_val_natural = '2321'
+        for x in vat:
+            check_val_natural += x if x.isdigit() else char_to_int[x]
+        if int(check_val_natural) % 97 == 1:
+            return True
+
+        # Company => weighted(9->2) mod11 on bsn
+        vat = vat[:-3]
+        if vat.isdigit() and checksum(vat) == 0:
+            return True
+
+        return False
 
     # Norway VAT validation, contributed by Rolv Råen (adEgo) <rora@adego.no>
     def check_vat_no(self, vat):
